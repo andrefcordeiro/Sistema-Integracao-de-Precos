31258a6bb292b84f329f818edb011e91ebb87e2e
Bruno590 <90658180+Bruno590@users.noreply.github.com>
2021-12-12 19:15:52 -0300

    Update mercado_livre_spider.py

--- crawling/crawling/spiders/mercado_livre_spider.py
+++ crawling/crawling/spiders/mercado_livre_spider.py
@@ -8,7 +8,7 @@ def collect_perguntas(perguntas):
 
     for pergunta in perguntas:
         quotes.append({
-            'pergunta': perg_resp_container.xpath('*//span[@class="ui-pdp-color--BLACK ui-pdp-size--SMALL ui-pdp-family--REGULAR ui-pdp-qadb__questions-list__question__label"]/text()').get(),
+            'pergunta': perguntas.xpath('*//span[@class="ui-pdp-color--BLACK ui-pdp-size--SMALL ui-pdp-family--REGULAR ui-pdp-qadb__questions-list__question__label"]/text()').get(),
             'nome': '',
             'data_pegunta': '',
             'resposta': p.xpath(
